f6ae33c29343ca50f16954731965e0cf3e220006
Jensen Judkins <thesmelliestman@gmail.com>
2019-02-04 09:20:05 -0700

    Created basically the whole GUI

--- src/pokemon/view/PokedexPanel.java
+++ src/pokemon/view/PokedexPanel.java
@@ -1,6 +1,49 @@
 package pokemon.view;
 
-public class PokedexPanel
+import javax.swing.*;
+import pokemon.controller.PokedexController;
+import java.awt.Color;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+
+public class PokedexPanel extends JPanel
 {
 
+	private PokedexController appController;
+	private SpringLayout appLayout;
+	private JButton myButton;
+	
+	public PokedexPanel(PokedexController appController)
+	{
+		super();
+		this.appController = appController;
+		
+		setupPanel();
+		setupLayout();
+		setupListeners();
+	}
+	
+	private void setupPanel()
+	{
+		this.setLayout(appLayout);
+		this.add(myButton);
+		
+	}
+	
+	private void setupLayout()
+	{
+		appLayout.putConstraint(SpringLayout.WEST, myButton,174, SpringLayout.WEST, this);
+		appLayout.putConstraint(SpringLayout.SOUTH, myButton,-135, SpringLayout.SOUTH, this);
+	}
+	
+	private void setupListeners()
+	{
+		myButton.addActionListener(new ActionListener()
+		{
+	public void actionPerformed(ActionEvent mouseClick)
+	{
+		
+	}
+		});
+	}
 }
