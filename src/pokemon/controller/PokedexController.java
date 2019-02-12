aac40c4ca9746cdd16f333985139d4711fa1969c
Jensen Judkins <thesmelliestman@gmail.com>
2019-02-12 09:21:03 -0700

    Added runner

--- src/pokemon/controller/PokedexController.java
+++ src/pokemon/controller/PokedexController.java
@@ -35,4 +35,27 @@ public class PokedexController
 	{
 		
 	}
-}
+	
+	public void updatePokemon(int index, String[] data )
+	{
+		if (data.length == 5)
+		{
+			Pokemon current = getPokemonList.get(index);
+			current.setAttackPoints(Interger.parseInt(data[0]));
+			current.setEnhancementModifier(Double.parseDouble(data[1]));
+			current.setHealthPoints(Interger.parseInt(data[2]));
+			current.setName(data[3]);
+			current.setCanEvolve(Boolean.parseBoolean(data[4]));
+		}
+	}
+	
+	public String[] buildPokemodexText()
+	{
+		String[] names = new String [pokemonList.size()];
+		
+		for(int index = 0; index < pokemonList.size(); index++)
+		{
+			names[index] = pokemonList.get(index).getName();
+		}
+		return names;
+	}
